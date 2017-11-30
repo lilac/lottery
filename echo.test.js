4dc76a67e60501e4b6dd917763b8b6693a70ae09
lilac <cnJamesDeng@gmail.com>
2017-11-30 16:04:52 +0800

    Add functional test

--- echo.test.js
+++ echo.test.js
@@ -1,7 +1,9 @@
 /**
  * Created by junjun.deng on 30/11/2017.
  */
+const supertest = require('supertest');
 const echo = require('./echo');
+const app = require('./server');
 
 test('echo', async () => {
     const params = {msg: 'hi'};
@@ -9,4 +11,10 @@ test('echo', async () => {
 
     await echo(ctx, () => {});
     expect(ctx.body).toBe(params);
+});
+
+test('echo route', async () => {
+    const response = await supertest(app.callback()).get('/echo/hi');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({msg: 'hi'});
 });
